f7a8629f014d004460e0876df66e400d0f7e8aa5
Shivsharan <shivsharangiram16@gmail.com>
2024-06-01 11:56:01 +0530

    we are on Dev Branch

---
+++ Mul.java
@@ -0,0 +1,8 @@
+public class Mul {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+        int mul=a*b;
+        System.out.println("The Mul is= " + mul);
+    }
+}
